099966c93a31d75be6aebdfb1cdfb4866e3bf264
Olaiwonismail <olaiwonismail@gmail.com>
2025-01-31 07:53:46 -0800

    d

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify,Response,make_response
+from flask_cors import CORS
+from datetime import datetime
+import json
+app = Flask(__name__)
+
+CORS(app)
+app.config["JSON_SORT_KEYS"]=False
+
+@app.route('/',methods = ['GET','POST'])
+def home():
+    email = "olaiwonismail@gmail.com"
+    date = datetime.now().isoformat()
+    github_url = "https://github.com/Olaiwonismail/HNG12-Stage-0"
+    data = {
+        "email":email,
+        "current_datetime":date,
+        "github_url" : github_url
+    }
+    print(data)
+    # return Response(data,mimetype='application/json')
+    return data
+
+
+if __name__ == '__main__':
+    app.json.sort_keys = False
+    app.run(debug=True)
